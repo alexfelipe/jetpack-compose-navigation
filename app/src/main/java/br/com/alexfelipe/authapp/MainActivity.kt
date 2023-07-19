fe71db63c04984c49e98f9c5e8fa6ad97043abf5
Alex Felipe <alexfelipevieira@gmail.com>
2023-07-19 13:09:33 -0300

    customize the screen

--- app/src/main/java/br/com/alexfelipe/authapp/MainActivity.kt
+++ app/src/main/java/br/com/alexfelipe/authapp/MainActivity.kt
@@ -1,13 +1,19 @@
 package br.com.alexfelipe.authapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Password
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.alexfelipe.authapp.ui.theme.AuthAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +40,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AuthScreen()
+                    AuthScreen(onEnterClick = {
+                        Log.i("MainActivity", "onCreate: $it")
+                    })
                 }
             }
         }
@@ -42,7 +51,9 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AuthScreen() {
+fun AuthScreen(
+    onEnterClick: (User) -> Unit
+) {
     Column {
         var username by remember {
             mutableStateOf("")
@@ -54,13 +65,52 @@ fun AuthScreen() {
             value = username,
             onValueChange = { newValue ->
                 username = newValue
-            })
+            },
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            label = {
+                Text("Usuário")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "pessoa que representa usuário"
+                )
+            }
+        )
         TextField(
             value = password,
             onValueChange = {
                 password = it
-            }, visualTransformation = PasswordVisualTransformation())
-        Button(onClick = { /*TODO*/ }) {
+            },
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            label = {
+                Text("Senha")
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Password,
+                    contentDescription = "representação de senha"
+                )
+            }
+        )
+        Button(
+            onClick = {
+                onEnterClick(
+                    User(
+                        username,
+                        password
+                    )
+                )
+            },
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+        ) {
             Text(text = "Entrar")
         }
     }
@@ -74,7 +124,7 @@ fun AuthScreenPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            AuthScreen()
+            AuthScreen(onEnterClick = {})
         }
     }
 }
---
+++ app/src/main/java/br/com/alexfelipe/authapp/User.kt
@@ -0,0 +1,6 @@
+package br.com.alexfelipe.authapp
+
+data class User(
+    val username: String,
+    val password: String
+)
